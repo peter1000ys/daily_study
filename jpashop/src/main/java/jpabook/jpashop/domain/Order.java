df439047fca95043ddeb4d02d51e22c3c1a6463f
Cheon YoSeong <cyspeter1000@gmail.com>
2024-07-15 01:10:59 +0900

    Learn: API 페이징

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -33,6 +33,7 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
+
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
