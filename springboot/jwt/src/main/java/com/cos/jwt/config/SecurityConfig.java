0a7c933b630d1c842abd6f1fb0d9b6bf31b4578a
CheonYoSeong <cyspeter1000@gmail.com>
2024-07-19 17:02:11 +0900

    JWT

--- springboot/jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ springboot/jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,16 +1,19 @@
 package com.cos.jwt.config;
 
-import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.config.jwt.JwtAuthenticationFilter;
+import com.cos.jwt.config.jwt.JwtAuthorizationFilter;
+import com.cos.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.context.SecurityContextHolderFilter;
+
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -19,17 +22,27 @@ import org.springframework.web.filter.CorsFilter;
 public class SecurityConfig {
 
     private final CorsFilter corsFilter;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final UserRepository userRepository;
+
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManager authenticationManager = authenticationConfiguration.getAuthenticationManager();
+
+
         http.csrf(csrf -> csrf.disable());
         // 세션을 쓰지 않겠다!
         http.sessionManagement(session ->{
             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 });
-        http.addFilterBefore(new MyFilter1(), SecurityContextHolderFilter.class);
+//        http.addFilterBefore(new MyFilter1(), SecurityContextHolderFilter.class);
         // 서버는 CORS 정책에서 벗어날 수 있다.
         http.addFilter(corsFilter);
+        http.addFilter(new JwtAuthenticationFilter(authenticationManager)); // AuthenticationManager
+        http.addFilter(new JwtAuthorizationFilter(authenticationManager, userRepository));
         // formLogin 사용하지 않는다
         http.formLogin(formLogin -> formLogin.disable());
         http.httpBasic(httpBasic -> httpBasic.disable());
@@ -39,7 +52,11 @@ public class SecurityConfig {
             request.requestMatchers("/api/v1/admin/**").access("hasRole('ROLE_ADMIN')");
             request.requestMatchers("/").permitAll();
         });
+//        http.formLogin( formLogin ->
+//                formLogin.loginProcessingUrl("/login"));
 
         return http.build();
     }
+
+
 }
--- springboot/jwt/src/main/java/com/cos/jwt/controller/RestApiController.java
+++ springboot/jwt/src/main/java/com/cos/jwt/controller/RestApiController.java
@@ -1,13 +1,22 @@
 package com.cos.jwt.controller;
 
+import com.cos.jwt.model.User;
+import com.cos.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class RestApiController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @GetMapping("/")
     public String home() {
         return "<h1>home</h1>";
@@ -16,6 +25,34 @@ public class RestApiController {
     @PostMapping("token")
     public String token() {
         return "<h1>token</h1>";
+    }
+
+
+
+    @PostMapping("join")
+    public String join(@RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles("ROLE_USER"); // Role은 기본으로 설정
+        userRepository.save(user);
+        return "회원가입 완료";
+    }
+
+    // user권한 이상 접근 가능
+    @GetMapping("/api/v1/user")
+    public String user() {
+        return "user";
+    }
+
+    // 매니저 권한 이상 접근 가능
+    @GetMapping("/api/v1/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    // admin 권한만 접근 가능
+    @GetMapping("/api/v1/admin")
+    public String admin() {
+        return "admin";
     }
 }
 
